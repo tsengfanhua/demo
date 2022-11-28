dfc1161a9715c3e1a55438d97173436daf176e9e
tsengfanhua <fanhua1216@gmail.com>
2022-11-28 11:43:26 +0800

    add

--- src/main/java/com/github/tsengfanhua/Main.java
+++ src/main/java/com/github/tsengfanhua/Main.java
@@ -3,6 +3,6 @@ package com.github.tsengfanhua;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi world!");
-
+        System.out.println("Hi world!");
     }
 }
